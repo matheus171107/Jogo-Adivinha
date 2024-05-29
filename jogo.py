12e014e36f47d079849b53af4d1852b26896b452
Matheus de Souza <matheus07sza@gmail.com>
2024-05-29 16:03:41 -0300

    Código Pronto

--- jogo.py
+++ jogo.py
@@ -1,15 +1,21 @@
 import random
 
-numeroS = random.randint(1, 100)
-tentativas = 0
-Ntentativas = 0
+def jogo_aidivinhacao():
+    numeroS = random.randint(1, 100)
+    tentativas = 0
+    Ntentativas = 0
 
-while tentativas != numeroS:
-    tentativas = int(input('Digite seu palpite :'))
-    Ntentativas += 1
-    if tentativas < numeroS:
-        print("Digite um valor mais alto!")
-    elif tentativas > numeroS:
-        print("Digite um valor mais baixo!") 
+    while tentativas != numeroS:
+        try:
+            tentativas = int(input('Digite seu palpite :'))
+            Ntentativas += 1
+            if tentativas < numeroS:
+                print("Digite um valor mais alto!")
+            elif tentativas > numeroS:
+                print("Digite um valor mais baixo!") 
+        except ValueError:
+            print("Por Favor, digite um número valido!")
+    print(f'Parabéns! Você acertou, número: {numeroS} com {Ntentativas} tentativas')
 
-print(f'Você acertou, número: {numeroS} com {Ntentativas} tentativas')
+if __name__ == "__main__":
+    jogo_aidivinhacao()
